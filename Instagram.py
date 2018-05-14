8f306aa20f6ac24b11917a62ef551273f1666977
张彦 <zhangyan1@meizu.com>
2018-05-14 16:44:08 +0800

    # 从详情页获取媒体

--- Instagram.py
+++ Instagram.py
@@ -1,8 +1,12 @@
 # -*- coding: utf-8 -*-
 import hashlib
 import json
+import os
+import os.path
 import re
 import requests
+
+from config import LOGIN_ID, PASSWORD
 
 
 # 媒体信息，包括图片和视频
@@ -39,7 +43,11 @@ class Instagram(object):
     # sharedData 正则表达式，sharedData 包含 csrf token，username，媒体地址，...... 等信息
     shared_data_regex = r'<script type="text/javascript">\s*window._sharedData\s*=\s*({.*});\s*</script>'
 
-    def __init__(self, csrf_token='', rhx_gis='', username='', user_id='', login_id='', password=''):
+    def __init__(self, csrf_token='', rhx_gis='', username='', user_id='', login_id='', password='',
+                 root='c:\instagram'):
+
+        self.root = root
+
         self.csrf_token = csrf_token
         self.rhx_gis = rhx_gis
         self.username = username
@@ -55,6 +63,9 @@ class Instagram(object):
         self.session.proxies = Instagram.proxies
         self.session.headers[
             'User-Agent'] = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:60.0) Gecko/20100101 Firefox/60.0'
+
+        self.has_next_page = True
+        self.end_cursor = ''
 
         # 收藏列表
         self.saved = []
@@ -111,13 +122,16 @@ class Instagram(object):
     # 得到收藏列表
     def fetch_saved(self):
         saved_url = 'https://www.instagram.com/{0}/?__a=1'.format(self.username)
-        self.session.headers['X-Instagram-GIS'] = 'b6a16681b1f62218fead4991aeeea38b'
+        self.session.headers['X-Instagram-GIS'] = '588b1b8709fd1f5cb4de30b890d7a140'
+        # 'b6a16681b1f62218fead4991aeeea38b'
         r = self.session.get(url=saved_url, timeout=30)
         if r.status_code == 200:
             json_obj = r.json()
             graphql = json_obj['graphql']
             user = graphql['user']
             edges = user['edge_saved_media']['edges']
+            self.has_next_page = user['edge_saved_media']['has_next_page']
+            self.end_cursor = user['edge_saved_media']['end_cursor']
             for edge in edges:
                 node = edge['node']
                 media = Media(id=node['id'],
@@ -136,47 +150,80 @@ class Instagram(object):
         self.shared_data = s.group(1)
         json_obj = json.loads(self.shared_data, encoding='utf8')
         if media.multiple:
-            self.medias += self.get_multiple_medias(json_obj, media.video)
+            self.medias += self.get_multi_medias(json_obj)
         else:
-            self.medias += self.get_single_medias(json_obj, media.video)
+            self.medias += self.get_single_medias(json_obj)
 
-    def get_single_medias(self, json_obj, video):
+    def get_single_medias(self, json_obj):
         medias = []
         url_name = None
+        node = json_obj['entry_data']['PostPage'][0]['graphql']['shortcode_media']
+        video = (node['__typename'] == 'GraphVideo')
         if video:
             url_name = 'video_url'
         else:
             url_name = 'display_url'
-        node = json_obj['entry_data']['PostPage'][0]['graphql']['shortcode_media']
         medias.append(
-            Media(id=node['id'], url='https://www.instagram.com/p/{0}/?saved-by={1}'.format(node['shortcode']),
-                  video=media.video, multiple=media.multiple,
+            Media(id=node['id'],
+                  url='https://www.instagram.com/p/{0}/?saved-by={1}'.format(node['shortcode'], self.username),
+                  video=video,
                   download_url=node[url_name]))
         return medias
 
-    def get_multi_medias(self, json_obj, video):
+    def get_multi_medias(self, json_obj):
         medias = []
-        url_name = None
-        if video:
-            url_name = 'video_url'
-        else:
-            url_name = 'display_url'
         nodes = json_obj['entry_data']['PostPage'][0]['graphql']['shortcode_media']['edge_sidecar_to_children']['edges']
-        for node in nodes:
+        for node_t in nodes:
+            node = node_t['node']
+            video = (node['__typename'] == 'GraphVideo')
+            if video:
+                url_name = 'video_url'
+            else:
+                url_name = 'display_url'
             medias.append(
-                Media(id=node['id'], url='https://www.instagram.com/p/{0}/?saved-by={1}'.format(node['shortcode']),
-                      video=media.video, multiple=media.multiple,
+                Media(id=node['id'],
+                      url='https://www.instagram.com/p/{0}/?saved-by={1}'.format(node['shortcode'], self.username),
+                      video=video,
                       download_url=node[url_name]))
         return medias
 
+    def mkdir(self):
+        if not os.path.exists(self.root):
+            os.mkdir(self.root)
+        os.chdir(self.root)
+        if not os.path.exists(self.username):
+            os.mkdir(self.username)
+        os.chdir(self.username)
+
+    def download_medias(self):
+        for media in self.medias:
+            filename = media.id
+            if media.video:
+                filename += '.mp4'
+            else:
+                filename += '.jpg'
+            if os.path.exists(filename):
+                print('file already exists.pass')
+            else:
+                print('download file:' + filename)
+                r = self.session.get(media.download_url)
+                try:
+                    with open(filename, "wb") as file:
+                        file.write(r.content)
+                except Exception as e:
+                    if os.path.exists(filename):
+                        os.remove(filename)
+
 
 if __name__ == '__main__':
-    it = Instagram(login_id='goosebaobao@gmail.com', password='978507')
+    it = Instagram(login_id=LOGIN_ID, password=PASSWORD)
     it.access_home()
     it.login()
     it.access_saved()
     # it.gen_rhx_gis()
-    it.fetch_saved()
+    while it.has_next_page:
+        it.fetch_saved()
     for media in it.saved:
         it.get_medias(media)
-    print(it.medias)
+    it.mkdir()
+    it.download_medias()
